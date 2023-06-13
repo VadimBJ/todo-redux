f888132c1097834dcd425a1f542141703faa19c9
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-06-13 16:14:49 +0200

    fix status change

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import ToDo from "./features/todo/ToDo";
 
 function App(): JSX.Element {
+  
   return (
     <div className="App">
       <h1 className="h1title">To Do List</h1>
